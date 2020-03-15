3b61ff8c0ce19968397c911ab37f61d8e1286ea2
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2020-03-15 09:52:37 -0400

    added allowCrossDomain

--- src/index.js
+++ src/index.js
@@ -19,7 +19,22 @@ mongoose.connect(
 );
 
 app.use(cors());
+const allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+
+  if ("OPTIONS" == req.method) {
+    res.send(200);
+  } else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
