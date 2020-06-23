aa632508d29f4ef8ba3246c78d9e570b9d8ad52a
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2020-06-23 19:59:51 -0400

    test

--- config/multer.js
+++ config/multer.js
@@ -3,19 +3,19 @@ const crypto = require("crypto");
 const path = require("path");
 
 module.exports = {
-  // storage: multer.memoryStorage(),
+  storage: multer.memoryStorage(),
 
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, path.resolve(__dirname, "..", "src", "uploads"));
-    },
-    filename(request, file, cb) {
-      const hash = crypto.randomBytes(6).toString("hex");
-      const fileName = `${hash}-${file.originalname}`;
+  // storage: multer.diskStorage({
+  //   destination: function (req, file, cb) {
+  //     cb(null, path.resolve(__dirname, "..", "src", "uploads"));
+  //   },
+  //   filename(request, file, cb) {
+  //     const hash = crypto.randomBytes(6).toString("hex");
+  //     const fileName = `${hash}-${file.originalname}`;
 
-      cb(null, fileName);
-    },
-  }),
+  //     cb(null, fileName);
+  //   },
+  // }),
 
   limits: { fileSIze: 2 * 1024 * 1024 },
 
