ded8bbfbd02804cc0cc284d1521b43d16ba36ae9
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2020-07-18 10:45:17 -0400

    test cors

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const allowCrossDomain = function (req, res, next) {
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
-    "Content-Type, authorization, Content-Length, X-Requested-With"
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
   );
 
   if ("OPTIONS" == req.method) {
