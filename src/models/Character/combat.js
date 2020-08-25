8e290cce9ca51175b79e332487bf456083460cde
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2020-08-25 16:31:23 -0400

    added fitness

--- src/models/Character/combat.js
+++ src/models/Character/combat.js
@@ -1,10 +1,9 @@
 module.exports = {
-  totalLife: Number,
-  currentLife: Number,
+  totalLife: { type: Number, default: 1 },
+  currentLife: { type: Number, default: 1 },
   totalEssence: { type: Number, default: 10 },
   currentEssence: Number,
-  exaustion: { type: Number, default: 0 },
-  inspiration: { type: Number, default: 0 },
+  fitness: { type: Number, default: 0 },
   movementSpeed: String,
   attack: { type: Number, default: 0 },
   damage: { type: Number, default: 0 },
