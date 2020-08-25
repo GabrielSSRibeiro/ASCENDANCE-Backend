ffd8e189b0bec4058f1edf32cb7db0848b9b935d
Gabriel Ribeiro <49738156+Gab1235@users.noreply.github.com>
2020-08-25 19:15:40 -0400

    fixed currentEssence

--- src/models/Character/combat.js
+++ src/models/Character/combat.js
@@ -2,7 +2,7 @@ module.exports = {
   totalLife: { type: Number, default: 1 },
   currentLife: { type: Number, default: 1 },
   totalEssence: { type: Number, default: 10 },
-  currentEssence: Number,
+  currentEssence: { type: Number, default: 10 },
   fitness: { type: Number, default: 0 },
   movementSpeed: String,
   attack: { type: Number, default: 0 },
